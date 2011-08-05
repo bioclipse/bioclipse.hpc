cf9c901d48adbe7cbb5c2fb9012f402be5aedcc8
Samuel Lampa <samuel.lampa@rilnet.com>
2011-08-05 13:15:01 +0200

    Added Preference page, changed it's class  name

---
+++ plugins/net.bioclipse.hpc/src/net/bioclipse/hpc/preferences/HPCPreferencePage.java
@@ -0,0 +1,38 @@
+package net.bioclipse.hpc.preferences;
+
+import net.bioclipse.hpc.business.HPCManager;
+import net.bioclipse.hpc.business.HPCUtils;
+
+import net.bioclipse.hpc.Activator;
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.FontFieldEditor;
+import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.jface.preference.StringFieldEditor;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+public class HPCPreferencePage 
+	extends FieldEditorPreferencePage 
+	implements IWorkbenchPreferencePage {
+	
+	public HPCPreferencePage() {
+		super(FieldEditorPreferencePage.GRID);
+		setPreferenceStore(Activator.getDefault().getPreferenceStore());
+		setDescription("HPC Integration Preferences");
+	}
+
+	@Override
+	public void init(IWorkbench arg0) {
+	}
+
+	@Override
+	protected void createFieldEditors() {
+		StringFieldEditor hostName = new StringFieldEditor("hostname", "Hostname", getFieldEditorParent());
+		addField(hostName);
+		StringFieldEditor userName = new StringFieldEditor("username", "Username", getFieldEditorParent());
+		addField(userName);
+	}
+
+}
