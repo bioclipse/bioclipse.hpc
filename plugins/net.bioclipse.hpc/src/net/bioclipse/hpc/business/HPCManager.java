5f19949c40ab41737fffbdcfea420761742f2b28
Samuel Lampa <samuel.lampa@gmail.com>
2013-07-09 19:19:23 +0200

    Cleaned up imports

--- plugins/net.bioclipse.hpc/src/net/bioclipse/hpc/business/HPCManager.java
+++ plugins/net.bioclipse.hpc/src/net/bioclipse/hpc/business/HPCManager.java
@@ -10,55 +10,19 @@
  ******************************************************************************/
 package net.bioclipse.hpc.business;
 
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.wizard.WizardDialog;
-import org.eclipse.rse.shells.ui.RemoteCommandHelpers;
-import org.eclipse.rse.subsystems.files.core.subsystems.IRemoteFile;
-import org.eclipse.rse.subsystems.files.core.subsystems.RemoteFileEmpty;
-import org.eclipse.rse.subsystems.shells.core.model.SimpleCommandOperation;
-import org.eclipse.rse.subsystems.shells.core.subsystems.IRemoteCmdSubSystem;
-import org.eclipse.rse.ui.SystemBasePlugin;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.IObjectActionDelegate;
-import org.eclipse.ui.IWorkbenchPart;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.internal.Workbench;
 
 import net.bioclipse.hpc.Activator;
 import net.bioclipse.hpc.domains.application.HPCApplication;
-import net.bioclipse.hpc.domains.toolconfig.ToolConfigDomain;
-import net.bioclipse.hpc.views.JobInfoView;
-import net.bioclipse.hpc.views.ProjInfoView;
-import net.bioclipse.hpc.wizards.ExecuteCommandWizard;
 import net.bioclipse.managers.business.IBioclipseManager;
 
-import org.eclipse.rse.core.*;
-import org.eclipse.rse.core.model.IHost;
-import org.eclipse.rse.core.model.ISystemRegistry;
-import org.eclipse.rse.core.model.SystemStartHere;
-import org.eclipse.rse.core.subsystems.ISubSystem;
-import org.eclipse.rse.subsystems.shells.core.*;
-import org.eclipse.rse.subsystems.shells.core.subsystems.IRemoteCmdSubSystem;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 
 public class HPCManager implements IBioclipseManager {
 
-	private static final Logger logger = LoggerFactory.getLogger(HPCManager.class);
+	//private static final Logger logger = LoggerFactory.getLogger(HPCManager.class);
 	public final HPCApplication application = getApplication();
 
 	/**
