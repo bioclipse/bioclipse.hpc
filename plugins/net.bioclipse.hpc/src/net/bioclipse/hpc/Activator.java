d7759822dad2e71a35ee9a1055e65276a17ff980
Samuel Lampa <samuel.lampa@gmail.com>
2013-07-10 07:40:41 +0200

    Clean up in Activator.java

--- plugins/net.bioclipse.hpc/src/net/bioclipse/hpc/Activator.java
+++ plugins/net.bioclipse.hpc/src/net/bioclipse/hpc/Activator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010  Samuel Lampa <samuel.lampa@gmail.com>
+ * Copyright (c) 2010-2013  Samuel Lampa <samuel.lampa@gmail.com>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -10,22 +10,15 @@
  ******************************************************************************/
 package net.bioclipse.hpc;
 
-import net.bioclipse.hpc.business.IJavaScriptHPCManager;
-import net.bioclipse.hpc.business.IJavaHPCManager;
 import net.bioclipse.hpc.business.IHPCManager;
+import net.bioclipse.hpc.business.IJavaHPCManager;
+import net.bioclipse.hpc.business.IJavaScriptHPCManager;
 import net.bioclipse.hpc.domains.application.HPCApplication;
 import net.bioclipse.hpc.domains.application.HPCUtils;
-import net.bioclipse.hpc.domains.toolconfig.ToolConfigDomain;
-import org.eclipse.jface.dialogs.Dialog;
+
 import org.eclipse.jface.preference.IPreferenceStore;
-import org.eclipse.jface.preference.PreferenceStore;
-import org.eclipse.jface.preference.StringFieldEditor;
-import org.eclipse.jface.wizard.Wizard;
-import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
-import org.osgi.service.prefs.PreferencesService;
 import org.osgi.util.tracker.ServiceTracker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,23 +27,23 @@ import org.slf4j.LoggerFactory;
  * The Activator class controls the plug-in life cycle
  */
 public class Activator extends AbstractUIPlugin {
-
     // Domain specific Application object
     public static HPCApplication application;
-
-    private static final Logger logger = LoggerFactory.getLogger(Activator.class);
-
-    // The shared instance
+    // The shared instance of this object
     private static Activator plugin;
-
     // Trackers for getting the managers
-    private ServiceTracker javaFinderTracker;
-    private ServiceTracker jsFinderTracker;
+    @SuppressWarnings("rawtypes")
+	private ServiceTracker javaFinderTracker;
+    @SuppressWarnings("rawtypes")
+	private ServiceTracker jsFinderTracker;
+    // Logger for this class
+    @SuppressWarnings("unused")
+	private static final Logger logger = LoggerFactory.getLogger(Activator.class);    
     
-    public Activator() {
-    }
+    public Activator() {}
 
-    public void start(BundleContext context) throws Exception {
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+	public void start(BundleContext context) throws Exception {
         super.start(context);
         plugin = this;
         javaFinderTracker
@@ -68,11 +61,11 @@ public class Activator extends AbstractUIPlugin {
         
         // Create an application object
         application = new HPCApplication();
-        
-        HPCUtils.getApplication().readGalaxyToolConfigFiles();
-        
+       
+        // Set some default configurations
+        application.readGalaxyToolConfigFiles();        
         // Need to set this here to, since initializeDefaultPreferences() is not always run
-        HPCUtils.getApplication().setDefaultPreferences();
+        application.setDefaultPreferences();
         
         /*
          * TODO: Start with IStartup and org.eclipse.ui.startup instead:
@@ -91,16 +84,12 @@ public class Activator extends AbstractUIPlugin {
 
     public void stop(BundleContext context) throws Exception {
         plugin = null;
-        
-        // Null out the application
         application = null;
-        
         super.stop(context);
     }
 
     /**
      * Returns the shared instance
-     *
      * @return the shared instance
      */
     public static Activator getDefault() {
